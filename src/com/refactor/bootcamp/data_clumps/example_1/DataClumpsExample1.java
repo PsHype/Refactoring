d03dee65c7b8e1f46102e5b0fa9cfefc83a53682
shaoshuai <shshao@thoughtworks.com>
2017-11-02 13:10:09 +0800

    init DataClumps

---
+++ src/com/refactor/bootcamp/data_clumps/example_1/DataClumpsExample1.java
@@ -0,0 +1,13 @@
+package com.refactor.bootcamp.data_clumps.example_1;
+
+public class DataClumpsExample1 {
+
+    private String address;
+    private String city;
+    private String street;
+    private String phoneNo;
+    private String areaCode;
+    private String name;
+    private int age;
+
+}
---
+++ test/com/refactor/bootcamp/data_clumps/DataClumpsExample1Test.java
@@ -0,0 +1,7 @@
+package com.refactor.bootcamp.data_clumps;
+
+import static org.junit.Assert.*;
+
+public class DataClumpsExample1Test {
+
+}
