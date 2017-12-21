ffa18533ba6aa90b311deea0cece107b78ba2a3e
shaoshuai <shshao@thoughtworks.com>
2017-12-21 17:22:22 +0800

    tdd

--- src/main/java/com/tdd/kata/calculate_words/reference/WordProcessor.java
+++ src/main/java/com/tdd/kata/calculate_words/reference/WordProcessor.java
@@ -1,17 +1,5 @@
 package com.tdd.kata.calculate_words.reference;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
-
 public class WordProcessor {
 
     private final String words;
@@ -20,34 +8,9 @@ public class WordProcessor {
         this.words = words;
     }
 
-    public String formatResult() {
-        if (Strings.isNullOrEmpty(words)) {
-            return "";
-        }
-
-        ArrayList<Word> words = groupAndSortWords();
-        return Joiner.on(" \n ").join(words);
+    public String splitResult() {
+        return "";
     }
 
-    private ArrayList<Word> groupAndSortWords() {
-        List<Word> words = groupWords();
-        Stream<Word> sorted = words.stream().sorted(Comparator.comparingInt(Word::getCount).reversed());
-        return Lists.newArrayList(sorted.iterator());
-    }
-
-    private List<Word> groupWords() {
-        Map<String, Word> wordMaps = new HashMap<>();
-        List<String> result = Splitter.onPattern("\\s+").omitEmptyStrings().splitToList(words);
-
-        for (String w : result) {
-            if (wordMaps.containsKey(w)) {
-                Word word = wordMaps.get(w);
-                word.addCount();
-                continue;
-            }
-            wordMaps.put(w, new Word(w));
-        }
-        return Lists.newArrayList(wordMaps.values());
-    }
 
 }
--- src/test/java/com/tdd/kata/calculate_words/reference/WordProcessorTest.groovy
+++ src/test/java/com/tdd/kata/calculate_words/reference/WordProcessorTest.groovy
@@ -4,19 +4,12 @@ import spock.lang.Specification
 
 class WordProcessorTest extends Specification {
 
-    def "should split right"() {
-        given: 'various strings'
-        def processor = new WordProcessor(input)
-        expect: 'not throw exception and is expect result'
-        processor.formatResult() == output
-        where:
-        input          | output
-        null           | ""
-        "and me"       | "and 1 \n me 1"
-        "and and me"   | "and 2 \n me 1"
-        "and me me"    | "me 2 \n and 1"
-        "and  me"      | "and 1 \n me 1"
-        "  and  me   " | "and 1 \n me 1"
-
+    def "should split right given null string"() {
+        given: 'null'
+        def processor = new WordProcessor(null)
+        expect: "not throw exception"
+        processor.splitResult() == ""
     }
+
+
 }
