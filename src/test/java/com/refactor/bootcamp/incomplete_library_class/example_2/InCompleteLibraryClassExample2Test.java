900071d67c7538fc9e51366ee9206724b0675e55
shaoshuai <949412944@qq.com>
2020-11-19 21:59:47 +0800

    add new test case for InCompleteLibrary

--- src/test/java/com/refactor/bootcamp/incomplete_library_class/example_2/InCompleteLibraryClassExample2Test.java
+++ src/test/java/com/refactor/bootcamp/incomplete_library_class/example_2/InCompleteLibraryClassExample2Test.java
@@ -1,14 +1,15 @@
-package com.refactor.bootcamp.incomplete_library_class.example_1;
+package com.refactor.bootcamp.incomplete_library_class.example_2;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
 
-public class InCompleteLibraryClassExample1Test {
+public class InCompleteLibraryClassExample2Test {
+
     @Test
     public void should_return_billAmountDate() {
-        InCompleteLibraryClassExample1 date = new InCompleteLibraryClassExample1();
+        InCompleteLibraryClassExample2 date = new InCompleteLibraryClassExample2();
         Date billAmountDate = date.getBillAmountDate();
         Assert.assertEquals(billAmountDate, new Date(2016, 10, 2));
     }
