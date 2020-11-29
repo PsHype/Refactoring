0d6cee1a9fb3d44a4738807c321e25fce9bf7106
shaoshuai <949412944@qq.com>
2020-11-29 17:28:25 +0800

    add package-info.java for Shotgun Surgery

---
+++ src/main/java/com/refactor/bootcamp/shotgun_surgery/example_1/ShotgunSurgeryExample1.java
@@ -0,0 +1,5 @@
+package com.refactor.bootcamp.shotgun_surgery.example_1;
+
+public class ShotgunSurgeryExample1 {
+
+}
---
+++ src/test/java/com/refactor/bootcamp/shotgun_surgery/example_1/ShotgunSurgeryExample1Test.java
@@ -0,0 +1,7 @@
+package com.refactor.bootcamp.shotgun_surgery.example_1;
+
+import junit.framework.TestCase;
+
+public class ShotgunSurgeryExample1Test {
+
+}
